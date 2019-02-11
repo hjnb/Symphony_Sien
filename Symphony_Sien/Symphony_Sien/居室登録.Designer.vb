3af4eb32ed2ca5ce6eb75b479e7a259e026c2d1f
yoshitaka.nagai <yoshitaka7144@gmail.com>
2019-02-11 17:09:33 +0900

    居室登録作成

--- Symphony_Sien/Symphony_Sien/居室登録.Designer.vb
+++ Symphony_Sien/Symphony_Sien/居室登録.Designer.vb
@@ -22,8 +22,306 @@ Partial Class 居室登録
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox0 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox7 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ComboBox11 = New System.Windows.Forms.ComboBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ComboBox10 = New System.Windows.Forms.ComboBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ComboBox9 = New System.Windows.Forms.ComboBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.ComboBox8 = New System.Windows.Forms.ComboBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "さくら"
+        '
+        'ComboBox0
+        '
+        Me.ComboBox0.FormattingEnabled = True
+        Me.ComboBox0.Location = New System.Drawing.Point(80, 26)
+        Me.ComboBox0.Name = "ComboBox0"
+        Me.ComboBox0.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox0.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(80, 59)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "すいせん"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(80, 125)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox3.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "ふじ"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(80, 92)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox2.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 96)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 12)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "あやめ"
+        '
+        'ComboBox7
+        '
+        Me.ComboBox7.FormattingEnabled = True
+        Me.ComboBox7.Location = New System.Drawing.Point(80, 257)
+        Me.ComboBox7.Name = "ComboBox7"
+        Me.ComboBox7.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox7.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 261)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 12)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "ひまわり"
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Location = New System.Drawing.Point(80, 224)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox6.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(23, 228)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 12)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "ぼたん"
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(80, 191)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox5.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(23, 195)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(42, 12)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "なでしこ"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(80, 158)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox4.TabIndex = 9
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(23, 162)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(42, 12)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "あじさい"
+        '
+        'ComboBox11
+        '
+        Me.ComboBox11.FormattingEnabled = True
+        Me.ComboBox11.Location = New System.Drawing.Point(80, 389)
+        Me.ComboBox11.Name = "ComboBox11"
+        Me.ComboBox11.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox11.TabIndex = 23
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(23, 393)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 12)
+        Me.Label9.TabIndex = 22
+        Me.Label9.Text = "ききょう"
+        '
+        'ComboBox10
+        '
+        Me.ComboBox10.FormattingEnabled = True
+        Me.ComboBox10.Location = New System.Drawing.Point(80, 356)
+        Me.ComboBox10.Name = "ComboBox10"
+        Me.ComboBox10.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox10.TabIndex = 21
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(23, 360)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(41, 12)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "やぐるま"
+        '
+        'ComboBox9
+        '
+        Me.ComboBox9.FormattingEnabled = True
+        Me.ComboBox9.Location = New System.Drawing.Point(80, 323)
+        Me.ComboBox9.Name = "ComboBox9"
+        Me.ComboBox9.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox9.TabIndex = 19
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(23, 327)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(23, 12)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "ゆり"
+        '
+        'ComboBox8
+        '
+        Me.ComboBox8.FormattingEnabled = True
+        Me.ComboBox8.Location = New System.Drawing.Point(80, 290)
+        Me.ComboBox8.Name = "ComboBox8"
+        Me.ComboBox8.Size = New System.Drawing.Size(120, 20)
+        Me.ComboBox8.TabIndex = 17
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(23, 294)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(44, 12)
+        Me.Label12.TabIndex = 16
+        Me.Label12.Text = "はまなす"
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(77, 436)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(82, 32)
+        Me.btnUpdate.TabIndex = 24
+        Me.btnUpdate.Text = "更　新"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        '居室登録
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(230, 510)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.ComboBox11)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.ComboBox10)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ComboBox9)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ComboBox8)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.ComboBox7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ComboBox6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox0)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "居室登録"
         Me.Text = "居室登録"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox0 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox7 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ComboBox11 As ComboBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents ComboBox10 As ComboBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents ComboBox9 As ComboBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents ComboBox8 As ComboBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents btnUpdate As Button
 End Class
--- Symphony_Sien/Symphony_Sien/居室登録.vb
+++ Symphony_Sien/Symphony_Sien/居室登録.vb
@@ -1,3 +1,123 @@
 ﻿Public Class 居室登録
 
+    'フォーカス制御用
+    Private displayedFlg As Boolean = False
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    Public Sub New()
+        InitializeComponent()
+
+        Me.StartPosition = FormStartPosition.CenterScreen
+    End Sub
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub 居室登録_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'コンボボックス初期設定
+        initTextBox()
+
+        '居室データ表示
+        displayRoomData()
+
+    End Sub
+
+    ''' <summary>
+    ''' コンボボックス初期設定
+    ''' </summary>
+    Private Sub initTextBox()
+        '設定する利用者名リスト取得
+        Dim cnn As New ADODB.Connection
+        Dim rs As New ADODB.Recordset
+        Dim sql As String = "select Nam from UsrM order by Kana"
+        cnn.Open(TopForm.DB_Sien)
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        Dim namList As New List(Of String)
+        While Not rs.EOF
+            namList.Add(Util.checkDBNullValue(rs.Fields("Nam").Value))
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cnn.Close()
+
+        '取得したリストを配列に
+        Dim itemsArray() As String = namList.ToArray()
+
+        '各ボックスへセット
+        For i As Integer = 0 To 11
+            Dim cb As ComboBox = CType(Controls("ComboBox" & i), ComboBox)
+            cb.ImeMode = ImeMode.Hiragana
+            cb.Items.AddRange(itemsArray)
+        Next
+
+    End Sub
+
+    ''' <summary>
+    ''' 居室データ表示
+    ''' </summary>
+    Private Sub displayRoomData()
+        Dim cnn As New ADODB.Connection
+        Dim rs As New ADODB.Recordset
+        Dim sql As String = "select Nam from Room order by Num"
+        cnn.Open(TopForm.DB_Sien)
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        Dim count As Integer = 0
+        While Not rs.EOF
+            CType(Controls("ComboBox" & count), ComboBox).Text = Util.checkDBNullValue(rs.Fields("Nam").Value)
+            rs.MoveNext()
+            count += 1
+        End While
+        rs.Close()
+        cnn.Close()
+
+        Me.ActiveControl = ComboBox11
+    End Sub
+
+    ''' <summary>
+    ''' 更新ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        '既存データ削除
+        Dim cn As New ADODB.Connection()
+        cn.Open(TopForm.DB_Sien)
+        Dim cmd As New ADODB.Command()
+        cmd.ActiveConnection = cn
+        cmd.CommandText = "delete from Room"
+        cmd.Execute()
+
+        '登録
+        Dim rs As New ADODB.Recordset()
+        rs.Open("Room", cn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockOptimistic)
+        For i As Integer = 0 To 11
+            rs.AddNew()
+            rs.Fields("Num").Value = i
+            rs.Fields("Nam").Value = CType(Controls("ComboBox" & i), ComboBox).Text
+        Next
+        rs.Update()
+        rs.Close()
+        cn.Close()
+
+        MsgBox("更新しました。", MsgBoxStyle.Information)
+
+    End Sub
+
+    ''' <summary>
+    ''' ききょうボックスフォーカスイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub ComboBox11_GotFocus(sender As Object, e As EventArgs) Handles ComboBox11.GotFocus
+        '最初の一回だけ処理を行う
+        If displayedFlg = False Then
+            ComboBox11.SelectionStart = 0
+            ComboBox11.SelectionLength = 0
+            displayedFlg = True
+        End If
+    End Sub
 End Class
