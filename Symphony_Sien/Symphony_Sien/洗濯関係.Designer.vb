d2632b8bc4f4d8e77a4a391571ba8de9d8e38b19
yoshitaka.nagai <yoshitaka7144@gmail.com>
2019-02-25 03:18:43 +0900

    洗濯関係：フォームデザイン作成

--- Symphony_Sien/Symphony_Sien/洗濯関係.Designer.vb
+++ Symphony_Sien/Symphony_Sien/洗濯関係.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class 洗濯関係
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,128 @@ Partial Class 洗濯関係
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.unitListBox = New System.Windows.Forms.ListBox()
+        Me.dgvResident = New System.Windows.Forms.DataGridView()
+        Me.btnRegist = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.rbtnWashList = New System.Windows.Forms.RadioButton()
+        Me.rbtnType = New System.Windows.Forms.RadioButton()
+        Me.dgvSisetu = New System.Windows.Forms.DataGridView()
+        Me.dgvSibutu = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvResident, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvSisetu, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvSibutu, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'unitListBox
+        '
+        Me.unitListBox.BackColor = System.Drawing.SystemColors.Control
+        Me.unitListBox.Font = New System.Drawing.Font("MS UI Gothic", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.unitListBox.FormattingEnabled = True
+        Me.unitListBox.ItemHeight = 19
+        Me.unitListBox.Location = New System.Drawing.Point(18, 31)
+        Me.unitListBox.Name = "unitListBox"
+        Me.unitListBox.Size = New System.Drawing.Size(39, 194)
+        Me.unitListBox.TabIndex = 0
+        '
+        'dgvResident
+        '
+        Me.dgvResident.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvResident.Location = New System.Drawing.Point(80, 31)
+        Me.dgvResident.Name = "dgvResident"
+        Me.dgvResident.RowTemplate.Height = 21
+        Me.dgvResident.Size = New System.Drawing.Size(325, 203)
+        Me.dgvResident.TabIndex = 1
+        '
+        'btnRegist
+        '
+        Me.btnRegist.Location = New System.Drawing.Point(124, 308)
+        Me.btnRegist.Name = "btnRegist"
+        Me.btnRegist.Size = New System.Drawing.Size(75, 29)
+        Me.btnRegist.TabIndex = 2
+        Me.btnRegist.Text = "登録"
+        Me.btnRegist.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(198, 308)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 29)
+        Me.btnPrint.TabIndex = 3
+        Me.btnPrint.Text = "印刷"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'rbtnWashList
+        '
+        Me.rbtnWashList.AutoSize = True
+        Me.rbtnWashList.Location = New System.Drawing.Point(295, 308)
+        Me.rbtnWashList.Name = "rbtnWashList"
+        Me.rbtnWashList.Size = New System.Drawing.Size(95, 16)
+        Me.rbtnWashList.TabIndex = 4
+        Me.rbtnWashList.TabStop = True
+        Me.rbtnWashList.Text = "洗濯物リスト表"
+        Me.rbtnWashList.UseVisualStyleBackColor = True
+        '
+        'rbtnType
+        '
+        Me.rbtnType.AutoSize = True
+        Me.rbtnType.Location = New System.Drawing.Point(295, 333)
+        Me.rbtnType.Name = "rbtnType"
+        Me.rbtnType.Size = New System.Drawing.Size(95, 16)
+        Me.rbtnType.TabIndex = 5
+        Me.rbtnType.TabStop = True
+        Me.rbtnType.Text = "処理別一覧表"
+        Me.rbtnType.UseVisualStyleBackColor = True
+        '
+        'dgvSisetu
+        '
+        Me.dgvSisetu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSisetu.Location = New System.Drawing.Point(433, 31)
+        Me.dgvSisetu.Name = "dgvSisetu"
+        Me.dgvSisetu.RowTemplate.Height = 21
+        Me.dgvSisetu.Size = New System.Drawing.Size(94, 328)
+        Me.dgvSisetu.TabIndex = 6
+        '
+        'dgvSibutu
+        '
+        Me.dgvSibutu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSibutu.Location = New System.Drawing.Point(538, 31)
+        Me.dgvSibutu.Name = "dgvSibutu"
+        Me.dgvSibutu.RowTemplate.Height = 21
+        Me.dgvSibutu.Size = New System.Drawing.Size(94, 263)
+        Me.dgvSibutu.TabIndex = 7
+        '
+        '洗濯関係
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "洗濯関係"
+        Me.ClientSize = New System.Drawing.Size(650, 379)
+        Me.Controls.Add(Me.dgvSibutu)
+        Me.Controls.Add(Me.dgvSisetu)
+        Me.Controls.Add(Me.rbtnType)
+        Me.Controls.Add(Me.rbtnWashList)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnRegist)
+        Me.Controls.Add(Me.dgvResident)
+        Me.Controls.Add(Me.unitListBox)
+        Me.Name = "洗濯関係"
+        Me.Text = "洗濯物リスト表/処理別一覧表"
+        CType(Me.dgvResident, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvSisetu, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvSibutu, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents unitListBox As ListBox
+    Friend WithEvents dgvResident As DataGridView
+    Friend WithEvents btnRegist As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents rbtnWashList As RadioButton
+    Friend WithEvents rbtnType As RadioButton
+    Friend WithEvents dgvSisetu As DataGridView
+    Friend WithEvents dgvSibutu As DataGridView
 End Class
--- Symphony_Sien/Symphony_Sien/洗濯関係.vb
+++ Symphony_Sien/Symphony_Sien/洗濯関係.vb
@@ -1,3 +1,161 @@
 ﻿Public Class 洗濯関係
 
+    'ﾕﾆｯﾄ配列
+    Private unitArray() As String = {"森", "星", "空", "花", "月", "虹", "光", "丘", "風", "雪"}
+
+    '
+    Private cellValueChangeFlg As Boolean = False
+
+    ''' <summary>
+    ''' 行ヘッダーのカレントセルを表す三角マークを非表示に設定する為のクラス。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class dgvRowHeaderCell
+
+        'DataGridViewRowHeaderCell を継承
+        Inherits DataGridViewRowHeaderCell
+
+        'DataGridViewHeaderCell.Paint をオーバーライドして行ヘッダーを描画
+        Protected Overrides Sub Paint(ByVal graphics As Graphics, ByVal clipBounds As Rectangle,
+           ByVal cellBounds As Rectangle, ByVal rowIndex As Integer, ByVal cellState As DataGridViewElementStates,
+           ByVal value As Object, ByVal formattedValue As Object, ByVal errorText As String,
+           ByVal cellStyle As DataGridViewCellStyle, ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle,
+           ByVal paintParts As DataGridViewPaintParts)
+            '標準セルの描画からセル内容の背景だけ除いた物を描画(-5)
+            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, value,
+                     formattedValue, errorText, cellStyle, advancedBorderStyle,
+                     Not DataGridViewPaintParts.ContentBackground)
+        End Sub
+
+    End Class
+
+    Public Sub New()
+        InitializeComponent()
+
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+    End Sub
+
+    Private Sub 洗濯関係_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        initDgvNam()
+
+        'ﾕﾆｯﾄ配列セット
+        unitListBox.Items.AddRange(unitArray)
+
+        '森を選択
+        unitListBox.SelectedIndex = 0
+    End Sub
+
+    Private Sub initDgvNam()
+        Util.EnableDoubleBuffering(dgvResident)
+
+        With dgvResident
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .SelectionMode = DataGridViewSelectionMode.CellSelect
+            .MultiSelect = False
+            .BorderStyle = BorderStyle.None
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersHeight = 20
+            .RowTemplate.Height = 18
+            .RowHeadersWidth = 25
+            .EnableHeadersVisualStyles = False
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .RowTemplate.HeaderCell = New dgvRowHeaderCell() '行ヘッダの三角マークを非表示に
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .ShowCellToolTips = False
+        End With
+
+        '空行追加
+        Dim dt As New DataTable()
+        dt.Columns.Add("Check", GetType(Boolean))
+        dt.Columns.Add("Nam", GetType(String))
+        dt.Columns.Add("Biko", GetType(String))
+        For i = 0 To 9
+            Dim row As DataRow = dt.NewRow()
+            row(0) = False
+            row(1) = ""
+            row(2) = ""
+            dt.Rows.Add(row)
+        Next
+        dgvResident.DataSource = dt
+
+        '幅設定等
+        With dgvResident
+            With .Columns("Check")
+                .HeaderText = "ワ"
+                .Width = 23
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .DefaultCellStyle.BackColor = Color.White
+                .DefaultCellStyle.SelectionBackColor = Color.White
+            End With
+
+            With .Columns("Nam")
+                .HeaderText = "氏名"
+                .Width = 105
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+            End With
+
+            With .Columns("Biko")
+                .HeaderText = "備　考"
+                .Width = 170
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+            End With
+        End With
+
+        cellValueChangeFlg = True
+
+        dgvResident.CurrentCell.Selected = False
+
+    End Sub
+
+    Private Sub dgvResident_CellPainting(sender As Object, e As DataGridViewCellPaintingEventArgs) Handles dgvResident.CellPainting
+        '行ヘッダーかどうか調べる
+        If e.ColumnIndex < 0 AndAlso e.RowIndex >= 0 Then
+            'セルを描画する
+            e.Paint(e.ClipBounds, DataGridViewPaintParts.All)
+
+            '行番号を描画する範囲を決定する
+            'e.AdvancedBorderStyleやe.CellStyle.Paddingは無視しています
+            Dim indexRect As Rectangle = e.CellBounds
+            indexRect.Inflate(-2, -2)
+            '行番号を描画する
+            TextRenderer.DrawText(e.Graphics,
+                (e.RowIndex + 1).ToString(),
+                e.CellStyle.Font,
+                indexRect,
+                e.CellStyle.ForeColor,
+                TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            '描画が完了したことを知らせる
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub dgvResident_CellEnter(sender As Object, e As DataGridViewCellEventArgs) Handles dgvResident.CellEnter
+        dgvResident.BeginEdit(False)
+    End Sub
+
+    Private Sub dgvResident_CellValueChanged(sender As Object, e As DataGridViewCellEventArgs) Handles dgvResident.CellValueChanged
+        If cellValueChangeFlg AndAlso e.ColumnIndex = 0 Then
+            Dim cellChecked As Boolean = dgvResident(e.ColumnIndex, e.RowIndex).Value
+            If cellChecked Then
+                dgvResident(e.ColumnIndex, e.RowIndex).Style.BackColor = Color.Red
+                dgvResident(e.ColumnIndex, e.RowIndex).Style.SelectionBackColor = Color.Red
+            Else
+                dgvResident(e.ColumnIndex, e.RowIndex).Style.BackColor = Color.White
+                dgvResident(e.ColumnIndex, e.RowIndex).Style.SelectionBackColor = Color.White
+            End If
+        End If
+    End Sub
+
+    Private Sub dgvResident_CurrentCellDirtyStateChanged(sender As Object, e As EventArgs) Handles dgvResident.CurrentCellDirtyStateChanged
+        If dgvResident.CurrentCellAddress.X = 0 AndAlso dgvResident.IsCurrentCellDirty Then
+            'コミットする
+            dgvResident.CommitEdit(DataGridViewDataErrorContexts.Commit)
+        End If
+    End Sub
 End Class
